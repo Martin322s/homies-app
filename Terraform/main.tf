c298eb81e79538be4473d9c836eb32adebe81175
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-18 12:52:23 +0300

    azure linux web app and config

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,3 +23,32 @@ resource "azurerm_service_plan" "asp" {
   os_type             = "Linux"
   sku_name            = "F1"
 }
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "Linux Web App"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_administrator_login_username
+  administrator_login_password = var.sql_administrator_password
+}
