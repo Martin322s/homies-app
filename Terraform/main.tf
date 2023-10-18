0e45d79907d65c2b73b453e5441a2e6eaf01760c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-10-18 12:48:01 +0300

    azure service plan and rg

---
+++ Terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Martin13s18HomiesRG"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "HomiesServicePlan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
